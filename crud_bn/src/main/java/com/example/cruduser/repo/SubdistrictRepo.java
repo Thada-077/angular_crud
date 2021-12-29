af4738c202ec0beb5f95816617adf8dc777588fa
61122201077 <62263977+Thada-077@users.noreply.github.com>
2021-12-29 20:57:32 +0700

    Add files via upload

---
+++ crud_bn/src/main/java/com/example/cruduser/repo/SubdistrictRepo.java
@@ -0,0 +1,8 @@
+package com.example.cruduser.repo;
+
+
+import com.example.cruduser.model.ThSubdistrict;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubdistrictRepo extends JpaRepository<ThSubdistrict,Short> {
+}
---
+++ crud_bn/src/main/java/com/example/cruduser/services/UserService.java
@@ -0,0 +1,24 @@
+package com.example.cruduser.services;
+
+import com.example.cruduser.model.Login;
+import com.example.cruduser.repo.LoginRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private LoginRepo repo;
+    public Login saveUser(Login login) {
+        return repo.save(login);
+    }
+
+    public Login fetchUserByEmailId(String email) {
+        return repo.findByEmailId(email);
+    }
+
+    public Login fetchUserByEmailIdAndPassword(String email, String password) {
+        return repo.findByEmailIdAndPassword(email,password);
+    }
+}
