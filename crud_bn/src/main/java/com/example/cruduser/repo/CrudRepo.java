09ae8afc2cf22c01ae734314748f8c70f052366d
61122201077 <62263977+Thada-077@users.noreply.github.com>
2022-01-04 00:34:06 +0700

    Delete crud_bn directory

--- crud_bn/src/main/java/com/example/cruduser/repo/CrudRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.cruduser.repo;
-
-import com.example.cruduser.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrudRepo extends JpaRepository<User,Integer> {
-}
