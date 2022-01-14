851428bb74ad942208278891b89b5f5ee0378010
61122201077 <62263977+Thada-077@users.noreply.github.com>
2022-01-14 17:40:18 +0700

    Delete crud_bn directory

--- crud_bn/src/main/java/com/example/cruduser/repo/SubdistrictRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.cruduser.repo;
-
-
-import com.example.cruduser.model.ThSubdistrict;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubdistrictRepo extends JpaRepository<ThSubdistrict,Short> {
-}
